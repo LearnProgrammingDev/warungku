5521685f265c8365b7dc01bcc3731048e3f41e5a
Randz Code <learnprogramming65@gmail.com>
2025-07-29 00:48:23 +0700

    migration table users setup

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username')->unique()->fillable();
+            $table->string('password')->nullable();
+            $table->string('fullname');
+            $table->string('email')->unique()->fillable();
+            $table->string('phone');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->softDeletes();
             $table->timestamps();
         });
 
