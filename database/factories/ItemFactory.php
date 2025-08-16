de716ac933c073df907152b0d9cfca277c2e2d32
Randz Code <learnprogramming65@gmail.com>
2025-08-17 03:02:30 +0700

    factory seeder catergory and item

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Item>
+ */
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->numberBetween(1000, 100000),
+            'category_id' => Category::factory(), 
+            'image' => $this->faker->imageUrl(640, 480, 'food', true, 'Item', true),
+            'is_active' => $this->faker->boolean(),
+        ];
+    }
+}
