cd905c7247c0f60edcacbd50e6c374ecef6e4dde
Randz Code <learnprogramming65@gmail.com>
2025-08-17 02:37:55 +0700

    migration and seeder roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
@@ -13,12 +14,12 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         $roles = [
-            ['role_name', => 'admin', 'description' => 'Administrator'],
-            ['role_name', => 'cashier', 'description' => 'Kasir'],
-            ['role_name', => 'chef', 'description' => 'Koki'],
-            ['role_name', => 'customer', 'description' => 'Pelanggan']
+            ['role_name' => 'admin', 'description' => 'Administrator'],
+            ['role_name' => 'cashier', 'description' => 'Kasir'],
+            ['role_name' => 'chef', 'description' => 'Koki'],
+            ['role_name' => 'customer', 'description' => 'Pelanggan']
         ];
 
-        DB::tabel('roles')->insert($roles);
+        DB::table('roles')->insert($roles);
     }
 }
