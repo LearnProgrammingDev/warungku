8821033939a4de8a04978577ebb63c1448c6fdad
Randz Code <learnprogramming65@gmail.com>
2025-08-22 01:50:29 +0700

    menu controller index to acces view menus all

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,19 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Item extends Model
 {
     use SoftDeletes, HasFactory;
 
-    protected $fillable = ['name', 'description', 'price', 'category_id', 'img', 'is_active','updated_at', 'deleted_at'];
+    protected $fillable = ['name', 'description', 'price', 'category_id', 'img', 'is_active', 'updated_at', 'deleted_at'];
     protected $dates = ['deleted_at'];
     public function category()
     {
-        return $this->belongsTo(Category::class); // Relasi ke tabel categories dimana 1 item memiliki 1 kategori
+        return $this->belongsTo(Category::class, 'category_id'); // Relasi ke tabel categories dimana 1 item memiliki 1 kategori
     }
     public function orderItems()
     {
