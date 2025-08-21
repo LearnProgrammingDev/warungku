6be5ff8df99ce8e7fe05e527e4cb78ab671b709a
Randz Code <learnprogramming65@gmail.com>
2025-08-22 01:49:06 +0700

    update seeder item to generate img source

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -22,7 +22,14 @@ class ItemFactory extends Factory
             'category_id' => $this->faker->numberBetween(1, 2),
             'description' => $this->faker->text(),
             'price' => $this->faker->randomFloat(2, 1000, 100000),
-            'img' => $this->faker->imageUrl(),
+            'img' => fake()->randomElement(
+                [
+                    'https://images.unsplash.com/photo-1591325418441-ff678baf78ef',
+                    'https://images.unsplash.com/photo-1663569820326-fece03afdf1c',
+                    'https://images.unsplash.com/photo-1611143669185-af224c5e3252',
+                    'https://images.unsplash.com/photo-1727726672422-0fff852450f9'
+                ]
+            ),
             'is_active' => $this->faker->boolean(),
         ];
     }
